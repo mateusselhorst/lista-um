6e2ba1a2230951229be494375f273460f9e772da
mateusselhorst <mateus_selhorst@estudante.sesisenai.org.br>
2025-07-09 09:13:22 -0300

    UPDATE- exercicio 16 pois era facil

---
+++ exercicio16.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Positivo,Negativo ou Zero</title>
+</head>
+
+<body>
+    <form method="POST" action="">
+        <label for="nome">Insira seu nome:</label>
+        <input type="text" id="nome" name="nome" required><br><br>
+                <label for="idade">Insira sua idade:</label>
+        <input type="number" id="idade" name="idade" required><br><br>
+        <button type="submit" name="verificar_idade">Verificar</button>
+    </form>
+
+    <?php
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['verificar_idade'])) {
+            $nome = $_POST['nome'];
+            $idade = $_POST['idade'];
+        };
+        if($idade < 17){
+            echo 'Não pode votar';
+        }else if($idade > 17){
+            echo 'Pode votar';
+        }
+    };
+
+    ?>
